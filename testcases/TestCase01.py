8df1f6cb1ffd4908ff52c8db34f98d0f06c4bdae
pretty.sanwale <pretty.sanwale@servicenow.com>
2020-06-12 19:34:02 +0530

    Test:AddedTCandUtilforstringprogram

---
+++ testcases/TestCase01.py
@@ -0,0 +1,16 @@
+from utilclass.Util01 import *
+# Note : If any function does not return any value then by default it will return None in python if we print it.
+
+
+class TestCase01:
+    def __init__(self):
+        pass
+    data = reverse_string("pretty beware sanwale")
+    print(data)
+
+    flag = count_substring('get***set**get**', "get", "set")
+    print(flag)
+
+    print(convert_case_of_string("pretty"))
+
+    print(to_find_index('On the other hand, you have different fingers.', 'hand'))
